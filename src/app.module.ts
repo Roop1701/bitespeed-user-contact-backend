1dd1024df54cedc1506208ce8e83b17bb6fa92f3
chatterjee.roopkumar17 <chatterjee.roopkumar17@gmail.com>
2024-07-21 14:44:03 +0530

    Removed unwanted files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './core/database/database.module';
 import { UserModule } from './services/user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
