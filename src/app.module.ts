779929e844dda8a5aceca6a6d2f83bc706284396
chatterjee.roopkumar17 <chatterjee.roopkumar17@gmail.com>
2024-07-21 17:04:02 +0530

    Added base file to check deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './core/database/database.module';
 import { UserModule } from './services/user/user.module';
@@ -9,7 +11,7 @@ import { UserModule } from './services/user/user.module';
     DatabaseModule,
     UserModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
